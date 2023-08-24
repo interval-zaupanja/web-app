fcde84eba5be4489ff23081549b4931ab2ab4214
interval-zaupanja <138260029+interval-zaupanja@users.noreply.github.com>
2023-08-25 00:34:08 +0200

    Popravljen TextImageBubble tako, da height: auto ne povzroča več težav

--- src/components/TextImageBubble.vue
+++ src/components/TextImageBubble.vue
@@ -1,13 +1,24 @@
+/*
+Dejansko imamo dva različna tipa mehurčka, saj se stilistična pravila spreminjajo glede na to, ali mehurček vsebuje sliko:
+  - če je ne vsebuje, se text pozicionira na sredino mehurčka
+  - če jo vsebuje, text zasede toliko prostora na vrhu, da se v celoti izpiše, preostali prostor pa zasede slika: posledično je text vedno na vrhu in le centriran, ni pa na sredini mehurčka
+
+*/
+
 <template>
-    <div class="bubble yellow-gray">
+    <div v-if="this.image && this.text" class="bubble yellow-gray">
         <div class="cell-container">
             <b>{{this.text}}</b>
         </div>
-        <div class="cell-container img-cell-container">
-            <div class="img-container">
-                <img :src="this.image"/>
-            </div>
+        <div v-if="this.image" class="img-cell-container">
+            <img class="quick-fix" :src="this.image"/>
         </div>
+    </div>
+    <div v-else-if="this.text" class="bubble yellow-gray vertical-center">
+        <b>{{this.text}}</b>
+    </div>
+    <div v-else class="bubble yellow-gray vertical-center">
+        <img :src="this.image"/>
     </div>
 </template>
 
@@ -22,7 +33,7 @@ export default {
 /* bubble-outer mora biti nastavljen na najvišji ravni (izven komponente), sicer inherit povzroča overflow iz div */
 
 * {
-    text-align: center;
+    text-align: center; /* horizontalno centrira tudi sliko, če se nahaja v bločnem vsebniku (div) */
     font-size: 20px;
 }
 
@@ -32,27 +43,34 @@ export default {
     display: table; /* !!! */
 }
 
+.vertical-center {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
 .cell-container {
     display: table-row;
     height: 0;
 }
 
 .img-cell-container {
+    display: table-row;
     height: auto;
+    position: relative;
 }
 
-.img-container {
-    height: auto;
-    width: 160px;
-    /* display: flex;
-    justify-content: center;
-    align-items: center; */
+.img-cell-container img { /* rešitev za centriranje znotraj div, ki ima dinamično generirano (auto) višino */
+    position: absolute;
+    z-index: 1;
+    left: 0px;
 }
 
 img {
-    max-height: 80px;
-    max-width: 160px;
-    margin-left: auto;
-    margin-right: auto;
+    width: 100%;
+    height: 100%;
+    object-fit: contain;
 }
+
+/* sliko bi lahko centrirali tudi z margin: auto, vendar ta deluje le na bločnih elementih, zato bi morali img pretvoriti v block */ 
 </style>
