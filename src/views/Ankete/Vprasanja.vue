a6c5b0a89f6d8a25201cab2886fdcc7f5d5280c9
interval-zaupanja <138260029+interval-zaupanja@users.noreply.github.com>
2023-09-05 01:06:34 +0200

    Popravek tortnega diagrama z mandati

--- src/views/Ankete/Vprasanja.vue
+++ src/views/Ankete/Vprasanja.vue
@@ -70,36 +70,38 @@ export default {
                 data: []
             }
             for (let i = 0; i < vprasanje.odgovori.length; i++) {
-                // LABELS
-                if (vprasanje.odgovori[i].tip === 'O' && vprasanje.odgovori[i].udelezba_tip === 'NBG') {
-                    podatki.labels.push(this.vrniOdgovor('NBG', false, 1))
-                } else {
-                    podatki.labels.push(
-                        vprasanje.odgovori[i].odgovor_stranka_ime_kratica ??
-                        this.capitalization(vprasanje.odgovori[i].odgovor, 1) ??
-                        this.vrniOdgovor(vprasanje.odgovori[i].odgovor_std ?? vprasanje.odgovori[i].tip, false, 1) ?? // ali ima primat odgovor ali tip lahko, da bo treba pri nekaterih odgovorih potrebno spremeniti
-                        vprasanje.odgovori[i].odgovor
-                    )
-                }
-
-                // COLORS
-                if (vprasanje.odgovori[i].tip === 'O' && vprasanje.odgovori[i].udelezba_tip === 'NBG') {
-                    podatki.backgroundColor.push(this.vrniStdBarvo('NBG'))
-                } else {
-                    podatki.backgroundColor.push(
-                        vprasanje.odgovori[i].odgovor_stranka_barva ??
-                        this.vrniStdBarvo(
-                            vprasanje.odgovori[i].odgovor_std ??
-                            vprasanje.odgovori[i].tip
+                if (podatek != 'st_mandatov_izvajalec' || (podatek === 'st_mandatov_izvajalec' && vprasanje.odgovori[i].st_mandatov_izvajalec)) {
+                    // LABELS
+                    if (vprasanje.odgovori[i].tip === 'O' && vprasanje.odgovori[i].udelezba_tip === 'NBG') {
+                        podatki.labels.push(this.vrniOdgovor('NBG', false, 1))
+                    } else {
+                        podatki.labels.push(
+                            vprasanje.odgovori[i].odgovor_stranka_ime_kratica ??
+                            this.capitalization(vprasanje.odgovori[i].odgovor, 1) ??
+                            this.vrniOdgovor(vprasanje.odgovori[i].odgovor_std ?? vprasanje.odgovori[i].tip, false, 1) ?? // ali ima primat odgovor ali tip lahko, da bo treba pri nekaterih odgovorih potrebno spremeniti
+                            vprasanje.odgovori[i].odgovor
                         )
-                    )
-                }
+                    }
 
-                // DATA
-                if (podatek === 'procent_izvajalec') {
-                    podatki.data.push(vprasanje.odgovori[i].procent_izvajalec)
-                } else if (podatek === 'st_mandatov_izvajalec') {
-                    podatki.data.push(vprasanje.odgovori[i].st_mandatov_izvajalec)
+                    // COLORS
+                    if (vprasanje.odgovori[i].tip === 'O' && vprasanje.odgovori[i].udelezba_tip === 'NBG') {
+                        podatki.backgroundColor.push(this.vrniStdBarvo('NBG'))
+                    } else {
+                        podatki.backgroundColor.push(
+                            vprasanje.odgovori[i].odgovor_stranka_barva ??
+                            this.vrniStdBarvo(
+                                vprasanje.odgovori[i].odgovor_std ??
+                                vprasanje.odgovori[i].tip
+                            )
+                        )
+                    }
+
+                    // DATA
+                    if (podatek === 'procent_izvajalec') {
+                        podatki.data.push(vprasanje.odgovori[i].procent_izvajalec)
+                    } else if (podatek === 'st_mandatov_izvajalec') {
+                        podatki.data.push(vprasanje.odgovori[i].st_mandatov_izvajalec)
+                    }
                 }
             }
             return podatki;
