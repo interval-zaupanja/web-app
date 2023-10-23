5c0fe8b3410f9f6b0f8e53c579b3ce8059558bf3
interval-zaupanja <138260029+interval-zaupanja@users.noreply.github.com>
2023-10-23 13:34:12 +0200

    Skripte za predelavo podatkov postavljene v ločeno mapo

--- src/views/Ankete/Vprasanja.vue
+++ src/views/Ankete/Vprasanja.vue
@@ -87,6 +87,8 @@ import DoughnutChart from '@/components/charts/DoughnutChart.vue'
 
 import Odgovori from '@/views/Ankete/Odgovori.vue'
 
+import { predelajOdgovore } from '@/scripts/predelajOdgovore.js'
+
 export default {
     name: 'Vprasanja',
     props: ['data', 'id'],
@@ -108,56 +110,7 @@ export default {
         this.loaded = true
     },
     methods: {
-        predelajOdgovore(vprasanje, podatek) {
-            const podatki = {
-                labels: [],
-                backgroundColor: [],
-                data: []
-            }
-            for (let i = 0; i < vprasanje.odgovori.length; i++) {
-                if (podatek != 'st_mandatov_izvajalec' || (podatek === 'st_mandatov_izvajalec' && vprasanje.odgovori[i].st_mandatov_izvajalec)) {
-                    // LABELS
-                    if (vprasanje.odgovori[i].tip === 'O' && vprasanje.odgovori[i].udelezba_tip === 'NBG') {
-                        podatki.labels.push(this.vrniOdgovor('NBG', false, 1))
-                    } else {
-                        podatki.labels.push(
-                            vprasanje.odgovori[i].odgovor_stranka_ime_kratica ??
-                            vprasanje.odgovori[i].odgovor_stranka_ime ??
-                            this.capitalization(vprasanje.odgovori[i].odgovor, 1) ??
-                            this.vrniOdgovor(vprasanje.odgovori[i].odgovor_std ?? vprasanje.odgovori[i].dolocnost_tip ?? vprasanje.odgovori[i].tip, false, 1) ?? // ali ima primat odgovor ali tip lahko, da bo treba pri nekaterih odgovorih potrebno spremeniti
-                            vprasanje.odgovori[i].odgovor
-                        )
-                    }
-
-                    // COLORS
-                    if (vprasanje.odgovori[i].tip === 'O' && vprasanje.odgovori[i].udelezba_tip === 'NBG') {
-                        podatki.backgroundColor.push(this.vrniStdBarvo('NBG'))
-                    } else {
-                        podatki.backgroundColor.push(
-                            vprasanje.odgovori[i].barva ??
-                            vprasanje.odgovori[i].odgovor_stranka_barva ??
-                            this.vrniStdBarvo(
-                                vprasanje.odgovori[i].odgovor_std ??
-                                vprasanje.odgovori[i].dolocnost_tip ??
-                                vprasanje.odgovori[i].tip
-                            )
-                        )
-                    }
-
-                    // DATA
-                    if (podatek === 'procent_izvajalec') {
-                        podatki.data.push(vprasanje.odgovori[i].procent_izvajalec)
-                    } else if (podatek === 'st_mandatov_izvajalec') {
-                        podatki.data.push(vprasanje.odgovori[i].st_mandatov_izvajalec)
-                    }
-                }
-            }
-            if (podatki.labels.length == 0) {
-                return false // potrebno, da ne prikažemo graf brez podatkov
-            } else {
-                return podatki
-            }
-        },
+        predelajOdgovore,
         kvalitativnaMeritev(oznaka) {
             switch (oznaka) {
                 case 'izid':
@@ -181,8 +134,8 @@ export default {
                 for (let j = 0; j < vprasanje.glasovanja_id.length; j++) {
                     const { data } = await axios.get(this.apiServer + "/api/glasovanja/" + vprasanje.glasovanja_id[j])
                     vprasanje.glasovanja.push({
-                      _id: vprasanje.glasovanja_id[j],
-                      ime: data.ime
+                        _id: vprasanje.glasovanja_id[j],
+                        ime: data.ime
                     })
                 }
             }
