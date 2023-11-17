4cc983bb6c738575e45261970ebda9118497b6c9
interval-zaupanja <138260029+interval-zaupanja@users.noreply.github.com>
2023-11-17 09:30:50 +0100

    Popravki pri odgovorih

--- src/views/Ankete/Odgovori.vue
+++ src/views/Ankete/Odgovori.vue
@@ -4,40 +4,46 @@
         <div v-for="odgovor in this.odgovori" :key="odgovor._id" class="bubble bubble-list yellow-gray">
             <span class="anchor-inner" :id="odgovor._id"></span>
             <div>
-                <div style="float: right">
-                    <div style="float: right; display: block">
+                <span style="float: right">
+                    <div v-if="odgovor.odgovor_stranka_logo_uri != null">
+                        <img :src="odgovor.odgovor_stranka_logo_uri" @click="$router.push('/stranke/' + odgovor.stranka_id)" style="max-height: 40px; max-width: 160px; float: right"/>
+                    </div>
+                    <div style="float: right; display: block; margin-top: 10px;">
                         <Report tip="odgovor" :id="odgovor._id" :pot="'ankete/' + this.id + '#' + odgovor._id"/>
                         <CopyLink :path="'ankete/' + this.id + '#' + odgovor._id" class="side-button"/>
                     </div>
-                    <div v-if="odgovor.odgovor_stranka_logo_uri != null">
-                        <img :src="odgovor.odgovor_stranka_logo_uri" @click="$router.push('/stranke/' + odgovor.stranka_id)" style="max-height: 40px; max-width: 160px; margin-top: 10px; float: right"/>
-                    </div>
-                </div>
+                </span>
                 <div>
-                    <span>
+                    <div class="odgovor">
+                        Odgovor:
                         <span v-if="odgovor.stranka_id">
-                            <p>
-                                Odgovor:
-                                <router-link :to="'/stranke/' + odgovor.stranka_id">
-                                    {{ odgovor.odgovor_stranka_ime }}
-                                    <span v-if="odgovor.odgovor_stranka_ime_kratica != null"> ({{ odgovor.odgovor_stranka_ime_kratica}})</span>
-                                </router-link>
-                            </p>
+                            <router-link :to="'/stranke/' + odgovor.stranka_id">
+                                {{ odgovor.odgovor_stranka_ime }}
+                                <span v-if="odgovor.odgovor_stranka_ime_kratica != null"> ({{ odgovor.odgovor_stranka_ime_kratica}})</span>
+                            </router-link>
                         </span>
-                        <span v-if="odgovor.odgovor || odgovor.odgovor_std">
-                            Odgovor: {{ this.vrniOdgovor(odgovor.odgovor ?? odgovor.odgovor_std, true, 1) ?? this.capitalization(odgovor.odgovor,1)}}
+                        <span v-else>
+                            {{
+                                this.capitalization(odgovor.odgovor, 0) ??
+                                this.vrniOdgovor(
+                                    odgovor.odgovor_std ??
+                                    odgovor.opredeljen_tip ??
+                                    odgovor.tip,
+                                    false, 0
+                                ) ??
+                                this.capitalization(odgovor.odgovor, 0)
+                            }}
                         </span>
-                    </span>
-                    <p v-if="!(odgovor.odgovor || odgovor.odgovor_std)">Tip odgovora: {{ this.vrniOdgovor(odgovor.tip, true, 0)}}</p>
-                    <p>
+                    </div>
+                    <div class="procenti">
                         {{ odgovor.procent_izvajalec }}%
                         <span v-if="(odgovor.procent_zgornja_meja_izvajalec - odgovor.procent_spodnja_meja_izvajalec) > 0">
                             ± {{ (odgovor.procent_zgornja_meja_izvajalec - odgovor.procent_spodnja_meja_izvajalec) / 2 }}%
                             ({{ odgovor.procent_spodnja_meja_izvajalec }}% - {{ odgovor.procent_zgornja_meja_izvajalec }}%)
                         </span>
-                    </p>
-                    <p v-if="odgovor.opis">Opis: {{ odgovor.opis}}</p>
-                    <p v-if="odgovor.opombe">Opombe: {{ odgovor.opombe}}</p>
+                    </div>
+                    <div v-if="odgovor.opis">Opis: {{ odgovor.opis}}</div>
+                    <div v-if="odgovor.opombe">Opombe: {{ odgovor.opombe}}</div>
                 </div>
             </div>
         </div>
