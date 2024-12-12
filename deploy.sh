0d8bfb89b82f64bbc5df11947c0624ee65e17253
interval-zaupanja <intervalzaupanja@gmail.com>
2024-12-12 10:21:40 +0100

    deploy.sh fixes

--- deploy.sh
+++ deploy.sh
@@ -20,9 +20,9 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:interval-zaupanja/interval-zaupanja.github.io.git main
+# git push -f git@github.com-interval-zaupanja:interval-zaupanja/interval-zaupanja.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:interval-zaupanja/web-app.git main:gh-pages
+git push -f git@github.com-interval-zaupanja:interval-zaupanja/web-app.git main:gh-pages
 
 cd -
